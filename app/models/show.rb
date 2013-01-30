067b30b45dfe754f3b27788aab30862c30a081f6
naathyn <nathan3k@gmail.com>
2013-01-30 00:43:40 -0600

    Formatting shows table

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   attr_accessible :date, :time, :venue, :address, :price
 
-  validates_numericality_of :price, allow_nil: true
+  validates_numericality_of :price, allow_nil: true, allow_blank: true
 
   before_save :check_price
 
