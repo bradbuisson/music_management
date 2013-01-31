b328be6ad206147050833cd7ac61291d82fbfcc9
naathyn <nathan3k@gmail.com>
2013-01-30 21:13:38 -0600

    Added timestamp helper

--- app/helpers/timestamp_helper.rb
+++ app/helpers/timestamp_helper.rb
@@ -1,5 +1,9 @@
 module TimestampHelper
   def timestamp
-    self.created_at.to_formatted_s(:long).gsub(/\d*:\d*/, '')
+    timestamp = "Posted on #{self.created_at.to_formatted_s(:long).gsub(/\d*:\d*/, '')}"
+    day = "#{timestamp.match(/\d{1}+/).to_s}".to_i.ordinalize
+    month = timestamp.gsub(/[\d,]/, '')
+    year = timestamp.match(/\d{4}/).to_s
+    "#{month} #{day}, #{year}"
   end
 end
