f038b823b8a769f8581a4a99b9ae5794f7494770
naathyn <nathan3k@gmail.com>
2013-02-02 08:54:31 -0600

    first look at putting the pieces together

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
 
   def home
     @title = "Home"
+    @articles = Article.paginate(page: params[:page], per_page: 4)
+    @shows = Show.paginate(page: params[:page], per_page: 3)
+    @blog = Blog.published.sort_by(&:updated_at).last
   end
 end
